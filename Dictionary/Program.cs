9d2219bc2241c24f0ca519719167458981fa4fe8
R560 <R560@DESKTOP-SONNG5O>
2021-01-25 02:33:04 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> student = new Dictionary<int, string>();
+            student.Add(1,"Ahmet");
+            student.Add(2,"Mehmet");
+            student.Add(3,"Engin");
+            student.Add(4,"Mustafa");
+        }
+    }
+}
